60759a61690d2256965c584774f72f708504b813
unknown <contact@leokieffer.com>
2023-09-22 16:11:38 +0200

    init

---
+++ testing_stuff.py
@@ -0,0 +1,14 @@
+"""
+Nr. Servername Severadress Port
+1 home  homeserver.com  8898
+"""
+print('{0:<4}{1:<19}{2:<25}{3:<5}'.format("Nr.", "Servername", "Serveradress", "Port"))
+print('{0:<4}{1:<19}{2:<25}{3:<5}'.format("1.", "teddy", "teddy.com", "6666"))
+print('{0:<4}{1:<19}{2:<25}{3:<5}'.format("2.", "mr.awesome", "192.168.0.15", "8898"))
+
+
+
+
+
+
+
